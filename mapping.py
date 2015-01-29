618c0bb2dad5b17c3de448aa3c9b7a0d595aecdd
nasser al-wohaibi <noonon@gmail.com>
2015-01-29 19:48:11 +0300

    test

---
+++ mapping.py
@@ -0,0 +1,46 @@
+﻿# -*- coding: utf-8 -*-
+#
+
+# dict of the main mapping of Python English keywords to Arabic keywords.
+keyword_mapping = {
+'and' : [u'و'],
+'array' : صف أو سرد أو مجموعة أو قائمة
+'as' : كانما أو كانه أو كأنها
+'assert' : تأكد , تأكد من , تأكد من أن , 
+'break' : توقف
+'class' : فصيلة , صنف, عائلة
+'continue:  تعدى , تجاوز
+'def' : عرف, تعريف
+'del' : احذف, اهمل , أزل
+'dict' : قائمة او قاموس أو فهرس أو سجل أو دليل
+'elif' : او اذا
+'else' : اخرا أو اخيرا أو أو 
+'except' : باستثناء  , استثني , استثن , اعترض , استثناء , إلا , ما لم , 
+'exec' : نفذ
+'finally' : أخيرا , أخرا
+'finaly' : أخيرا
+'for' : [u'لكل'],
+'from' : من
+'global' :
+'hashedarray' :
+'if' : اذا
+'import' : [''],
+'in' : في
+'is' : هو , هي ,
+'lambda' : دالة
+'library' : مكتبة
+'list' :
+'matrix' : مصفوفة
+'not' : ليس
+'or' : أو
+'pass' : تجاهل , تجاوز , تعدى , تعدى
+'print' : اطبع او اظهر او استخرج او اخرج اكتب
+'raise' : خطأ
+'return' : النتيجة , المحصلة , ارجع
+'try' : جرب
+'type' : نوع
+'while' : [u'طالما',u'بينما',u'ما دام',u'ما دامت'],
+'with' : مع , بإستخدام , 
+'yield' : انتج , اعط , اعطي , 
+
+}
